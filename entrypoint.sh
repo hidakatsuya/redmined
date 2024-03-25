3e612b33f87968725e7934a45f0f0820e49ef08f
Katsuya Hidaka <hidakatsuya@gmail.com>
2024-03-25 22:26:45 +0900

    Fix the permission error during bundle install

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,13 @@
 #!/bin/bash -e
 
-if [ -n "$USER_ID" ]; then
+if [ -n "$USER_ID" ] && [ -n "$GROUP_ID" ]; then
   sudo usermod -u $USER_ID $USER_NAME > /dev/null
-fi
-
-if [ -n "$GROUP_ID" ]; then
   sudo groupmod -g $GROUP_ID $USER_NAME > /dev/null
-fi
 
-exec setpriv --reuid=$USER_ID --regid=$GROUP_ID --init-groups --reset-env "$@"
+  export HOME=/home/$USER_NAME
+
+  exec setpriv --reuid=$USER_ID --regid=$GROUP_ID --init-groups "$@"
+else
+  exec "$@"
+fi
 
