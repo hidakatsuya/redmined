fbf2653e89d4b24b1ddc05dafaef20b66ee28260
Katsuya HIDAKA <hidakatsuya@gmail.com>
2024-03-26 13:02:36 +0900

    Fix the error with Docker for mac: groupadd: GID '20' already exists

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,13 @@
 #!/bin/bash -e
 
+export HOME=/home/$USER_NAME
+
+# If USER_ID and GROUP_ID are set, they will be set to the UID and GID of the developer user
 if [ -n "$USER_ID" ] && [ -n "$GROUP_ID" ]; then
   sudo usermod -u $USER_ID $USER_NAME > /dev/null
   sudo groupmod -g $GROUP_ID $USER_NAME > /dev/null
 
-  export HOME=/home/$USER_NAME
-
   exec setpriv --reuid=$USER_ID --regid=$GROUP_ID --init-groups "$@"
 else
-  exec "$@"
+  exec setpriv --reuid=$(id -u $USER_NAME) --regid=$(id -g $USER_NAME) --init-groups "$@"
 fi
-
