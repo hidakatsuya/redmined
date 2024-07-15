f07d2f4549cfb1ea1893be439ab835a9e5e0c964
Katsuya Hidaka <hidakatsuya@gmail.com>
2024-07-15 12:36:35 +0900

    Unify shell script indent size

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,10 +4,10 @@ export HOME=/home/$USER_NAME
 
 # If USER_ID and GROUP_ID are set, they will be set to the UID and GID of the developer user
 if [ -n "$USER_ID" ] && [ -n "$GROUP_ID" ]; then
-  sudo usermod -u $USER_ID $USER_NAME > /dev/null
-  sudo groupmod -g $GROUP_ID $USER_NAME > /dev/null
+    sudo usermod -u $USER_ID $USER_NAME > /dev/null
+    sudo groupmod -g $GROUP_ID $USER_NAME > /dev/null
 
-  exec setpriv --reuid=$USER_ID --regid=$GROUP_ID --init-groups "$@"
+    exec setpriv --reuid=$USER_ID --regid=$GROUP_ID --init-groups "$@"
 else
-  exec setpriv --reuid=$(id -u $USER_NAME) --regid=$(id -g $USER_NAME) --init-groups "$@"
+    exec setpriv --reuid=$(id -u $USER_NAME) --regid=$(id -g $USER_NAME) --init-groups "$@"
 fi
