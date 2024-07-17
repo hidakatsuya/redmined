f7a8df3efc37f8cfdfeb9bed55d0fd782d9c9f77
Katsuya Hidaka <hidakatsuya@gmail.com>
2024-07-18 02:20:49 +0900

    Add installation script

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -eu
+
+install() {
+    local dir=$1
+    if [ ! -d $dir ]; then
+        return 1
+    fi
+
+    wget -q -P $dir https://raw.githubusercontent.com/hidakatsuya/redmined/main/redmined
+    chmod +x $dir/redmined
+
+    echo "Installed redmined to $dir"
+}
+
+abort() {
+    local extra_message=$1
+    echo "Failed to install redmined: $extra_message"
+    exit 1
+}
+
+install ~/.local/bin || install ~/bin || abort "~/.local/bin and ~/bin are not found."
